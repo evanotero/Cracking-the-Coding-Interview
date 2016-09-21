16cdf66c2e66f280ff8d7052b4609acb9a24b4c9
evanotero <oteroev@bc.edu>
2016-09-21 19:11:59 -0400

    Finished RotateMatrix

--- src/Chapter_1/Question1_7/RotateMatrix.java
+++ src/Chapter_1/Question1_7/RotateMatrix.java
@@ -6,7 +6,31 @@
 package Chapter_1.Question1_7;
 
 public class RotateMatrix {
-    public static int[][] rotateMatrix(int[][] m) {
-        return null;
+    public static int[][] rotateMatrix(int[][] matrix) {
+        if (matrix.length == 0 || matrix.length != matrix[0].length)
+            return null;
+
+        int n = matrix.length;
+        for (int layer = 0; layer < n / 2; layer++) {
+            int first = layer, last = n - 1 - layer;
+            for (int i = first; i < last; i++) {
+                int offset = i - first;
+                int top = matrix[first][i];
+
+                // left -> top
+                matrix[first][i] = matrix[last - offset][first];
+
+                // bottom -> left
+                matrix[last - offset][first] = matrix[last][last - offset];
+
+                // right -> bottom
+                matrix[last][last - offset] = matrix[i][last];
+
+                // top -> right
+                matrix[i][last] = top;
+            }
+        }
+
+        return matrix;
     }
 }
--- src/Chapter_1/Question1_7/RotateMatrixTest.java
+++ src/Chapter_1/Question1_7/RotateMatrixTest.java
@@ -7,7 +7,31 @@ import static org.junit.Assert.*;
 public class RotateMatrixTest {
     @Test
     public void rotateMatrix() throws Exception {
+        int[][] matrix = new int[5][5];
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                matrix[i][j] = count++;
+            }
+        }
 
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.printf("%2d ", matrix[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        RotateMatrix.rotateMatrix(matrix);
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.printf("%2d ", matrix[i][j]);
+            }
+            System.out.println();
+        }
     }
 
 }
